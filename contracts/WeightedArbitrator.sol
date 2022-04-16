96ec2f42e0c9d82f0504ef5ebd761bac56a10053
gratestas <stasgrate@gmail.com>
2022-04-16 17:45:15 +0300

    updated NatSpec Format comments

--- contracts/WeightedArbitrator.sol
+++ contracts/WeightedArbitrator.sol
@@ -11,7 +11,7 @@ import "hardhat/console.sol";
  */
 contract WeightedArbitrator is IArbitrator, IArbitrable {
     address public owner = msg.sender;
-    uint256 public arbitratorFee;
+    uint256 public feePerArbitrator;
     IArbitrator[] public authorizedArbitrators;
     mapping(address => bool) public isAuthorizedArbitrator;
 
@@ -29,8 +29,8 @@ contract WeightedArbitrator is IArbitrator, IArbitrable {
 
     error InsufficientPayment(uint256 _available, uint256 _required);
 
-    constructor(uint256 _arbitratorFee, IArbitrator[] memory _arbitrators) {
-        arbitratorFee = _arbitratorFee;
+    constructor(uint256 _feePerArbitrator, IArbitrator[] memory _arbitrators) {
+        feePerArbitrator = _feePerArbitrator;
         _addArbitrators(_arbitrators);
     }
 
@@ -40,31 +40,30 @@ contract WeightedArbitrator is IArbitrator, IArbitrable {
         }
     }
 
-    /** @dev Execute a ruling of a dispute.
-     *  @param _arbitrator ID of the dispute in the Arbitrator contract.
+    /** @dev Add an arbitrator to the list of authorized arbitrators. Must be called only by owner.
+     *  @param _arbitrator address of the arbitrator contract.
      */
     function addArbitrator(IArbitrator _arbitrator) public onlyOwner {
         authorizedArbitrators.push(_arbitrator);
     }
 
+    /** @dev Return the total number of authorized arbitrators.
+     *  @return numberOfArbitrators
+     */
     function getNumberOfArbitrators() public view returns (uint256) {
         return authorizedArbitrators.length;
     }
 
-    /** @dev Execute a ruling of a dispute.
-     *  @param _index ID of the dispute in the Arbitrator contract.
+    /** @dev Return arbitrator by its index from the list of authorized arbitrators.
+     *  @param _index index of an arbitrator in the list.
      */
     function getArbitrator(uint256 _index) public view returns (IArbitrator) {
         return authorizedArbitrators[_index];
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "can only be called by the owner.");
-        _;
-    }
-
     /** @dev Create a dispute. Must be called by the arbitrable contract.
      *  Must be paid at least arbitrationCost().
+     *  a proxy call is invoked on each authorized arbitrator to create sub-dispute and store its ID in Dispute struct.
      *  @param _choices Amount of choices the arbitrator can make in this dispute. When ruling ruling<=choices.
      *  @param _extraData Can be used to give additional info on the dispute to be created.
      *  @return disputeID ID of the dispute created.
@@ -89,7 +88,7 @@ contract WeightedArbitrator is IArbitrator, IArbitrable {
         });
         for (uint256 i = 0; i < authorizedArbitrators.length; i++) {
             uint256 _subDisputeID = IArbitrator(authorizedArbitrators[i])
-                .createDispute{value: arbitratorFee}(_choices, _extraData);
+                .createDispute{value: feePerArbitrator}(_choices, _extraData);
             dispute.subDisputeIDs[i] = _subDisputeID;
         }
         disputes.push(dispute);
@@ -98,6 +97,11 @@ contract WeightedArbitrator is IArbitrator, IArbitrable {
         emit DisputeCreation(disputeID, IArbitrable(msg.sender));
     }
 
+    /** @dev Return the dispute bu its ID.
+     *  Must be paid at least arbitrationCost().
+     *  @param _disputeID ID of the dispute.
+     *  @return dispute
+     */
     function getDisputeByID(uint256 _disputeID)
         public
         view
@@ -106,11 +110,6 @@ contract WeightedArbitrator is IArbitrator, IArbitrable {
         return disputeByID[_disputeID];
     }
 
-    //********** */ to be implemented and tested
-
-    /*
-     ********  from Arbitrable*********
-     */
     function rule(uint256 _disputeID, uint256 _ruling) public override {
         DisputeStruct storage dispute = disputes[_disputeID];
         require(_ruling <= dispute.choices, "Invalid ruling.");
@@ -126,8 +125,10 @@ contract WeightedArbitrator is IArbitrator, IArbitrable {
         dispute.arbitrated.rule(_disputeID, _ruling);
     }
 
-    /*
-     ********  from Arbitrator *********
+    /**
+     * @dev Compute the overall cost of arbitration based on number of authorized arbitrators. It is recommended not to increase it often, as it can be highly time and gas consuming for the arbitrated contracts to cope with fee augmentation.
+     * @param _extraData Can be used to give additional info on the dispute to be created.
+     * @return cost Amount to be paid. O(number of arbitrators)
      */
     function arbitrationCost(bytes memory _extraData)
         public
@@ -135,9 +136,14 @@ contract WeightedArbitrator is IArbitrator, IArbitrable {
         override
         returns (uint256)
     {
-        return arbitratorFee * authorizedArbitrators.length;
+        return feePerArbitrator * authorizedArbitrators.length;
     }
 
+    /** @dev Cost of appeal. Since it is not possible, it's a high value which can never be paid.
+     *  @param _disputeID ID of the dispute to be appealed. Not used by this contract.
+     *  @param _extraData Not used by this contract.
+     *  @return fee Amount to be paid.
+     */
     function appealCost(uint256 _disputeID, bytes memory _extraData)
         public
         pure
@@ -189,5 +195,10 @@ contract WeightedArbitrator is IArbitrator, IArbitrable {
         returns (uint256 ruling)
     {
         ruling = disputes[_disputeID].ruling;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "can only be called by the owner.");
+        _;
     }
 }
